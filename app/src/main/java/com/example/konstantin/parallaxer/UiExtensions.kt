97ad3faf26719f298a99489684d35be815bfe8e2
konstantin <kk@mobileup.ru>
2018-05-07 20:09:59 +0300

    Optimize imports

--- app/src/main/java/com/example/konstantin/parallaxer/UiExtensions.kt
+++ app/src/main/java/com/example/konstantin/parallaxer/UiExtensions.kt
@@ -1,28 +1,13 @@
 package com.example.konstantin.parallaxer
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Context
 import android.content.res.Resources
-import android.graphics.Point
-import android.graphics.drawable.Drawable
 import android.os.Build
-import android.support.annotation.ColorRes
-import android.support.annotation.DrawableRes
 import android.support.annotation.LayoutRes
-import android.support.design.widget.CoordinatorLayout
-import android.support.v4.content.ContextCompat
-import android.support.v4.graphics.drawable.DrawableCompat
-import android.support.v4.graphics.drawable.TintAwareDrawable
 import android.text.Html
 import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
